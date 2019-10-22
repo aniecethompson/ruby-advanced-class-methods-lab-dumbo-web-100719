0b18ede60f028f740b1845c59bd35d16d9e7f41c
Aniece Thompson <aniecet@yahoo.com>
2019-10-22 01:22:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,29 @@ class Song
     self.class.all << self
   end
 
+  def self.create #class constructor
+    song = self.new
+    self.all << song
+    song
+  end
+  
+  def self.new_by_name(title) #class constructor
+    song = self.new
+    song.name = title
+    song
+  end
+  
+  def self.create_by_name(title)
+    
+    song =self.create
+    binding.pry
+    song.name= title
+    song
+  end
+ Song.create_by_name("Cool Title")
+  
+  def self.find_by_name(title)
+    result = self.all.detect{|song| song.name == title}
+    result
+  end
 end
